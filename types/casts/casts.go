9766cc283548b0735ff8b8b8c93ebdcc0264c90c
andrew <andrewnijmeh1@gmail.com>
2022-12-20 19:39:32 -0500

    (types): finish adding all type structs in go

--- types/casts/casts.go
+++ types/casts/casts.go
@@ -1,3 +1,5 @@
+package casts
+
 type CastRoot struct {
   Result Result `json:"result"`
   Next Next `json:"next"`
@@ -10,4 +12,69 @@ type Result struct {
 type Cast struct {
   Hash string `json:"hash"`
   ThreadHash string `json:"threadHash"`
+  Author Author `json:"author"`
+  Timestamp int64 `json:"timestamp"`
+  Replies Replies `json:"replies"`
+  Reactions Reactions `json:"reactions"`
+  Recasts Recasts `json:"recasts"`
+  Watches Watches `json:"watches"`
+  Recast bool `json:"recast"`
+  ViewerContext ViewerContext `json:"viewerContext"`
+}
+
+type Author struct {
+  Fid int64 `json:"fid"`
+  Username string `json:"username"`
+  DisplayName string `json:"displayName"`
+  Pfp PFP `json:"pfp"`
+  Profile Profile `json:"profile"`
+  FollowerCount int64 `json:"followerCount"`
+  FollowingCount int64 `json:"followingCount"`
+}
+
+type PFP struct {
+  URL string `json:"url"`
+  Verified bool `json:"verified"`
+}
+
+type Profile struct {
+  Bio Bio `json:"bio"`
+}
+
+type Bio struct {
+  Text string `json:"text"`
+}
+
+type Replies struct {
+  Count int32 `json:"count"`
+}
+
+type Reactions struct {
+  Count int32 `json:"count"`
+}
+
+type Recasts struct {
+  Count int32 `json:"count"`
+  Recasters []Recasters `json:"recastes"`
+}
+
+type Recasters struct {
+  Fid int64 `json:"fid"`
+  Username string `json:"username"`
+  DisplayName string `json:"displayName"`
+  RecastHash string `json:"recastHash"`
+}
+
+type ViewerContext struct {
+  Reacted bool `json:"reacted"`
+  Recast bool `json:"recast"`
+  Watched bool `json:"watched"`
+}
+
+type Watches struct {
+  Count int32 `json:"count"`
+}
+
+type Next struct {
+  Cursor string `json:"cursor"`
 }
